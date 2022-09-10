31b43610d82da8bbb7ba292408dca7171a57cc47
Dalerem <dalerem2022@gmail.com>
2022-09-10 16:48:53 -0300

    Atualizando Requisicão

--- app.py
+++ app.py
@@ -12,18 +12,27 @@ __Author__ = "Dálerem Teixeira"
 __Email__ = "dalerem2022@gmail.com"
 __Linkedin__ = "https://www.linkedin.com/in/d%C3%A1lerem-teixeira-949a48236/"
 
-class Requisicao:
+class Requisicao_api:
+    """
+        Classe de Requisição de api
+    """
+    def __init__(self, cot):
+        self.cot = cot
+    def dolar(self):
+        cotacao_dolar = self.cot["USDBRL"]["bid"]
+        return cotacao_dolar
+    def euro(self):
+        cotacao_euro = self.cot["EURBRL"]["bid"]
+        return cotacao_euro
+    def btc(self):
+        cotacao_btc = self.cot["BTCBRL"]["bid"]
+        return cotacao_btc
 
-    def get(self):
-        cotacoes = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
-        cotas = cotacoes.json()
+cotacoes = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
+cotas = cotacoes.json()
 
-        cotacao_dolar = cotas["USDBRL"]["bid"]
-        cotacao_euro = cotas["EURBRL"]["bid"]
-        cotacao_btc = cotas["BTCBRL"]["bid"]
+resp = Requisicao_api(cotas)
 
-        return cotacao_dolar, cotacao_euro, cotacao_btc
-
-resp = Requisicao()
-
-print("Cotações do Dolar, Euro e Btc:", resp.get())
+print("Cotações do Dolar:", resp.dolar())
+print("Cotações do Euro:", resp.euro())
+print("Cotações do Btc:", resp.btc())
